f5d56fde8a3596c851865cfb143b8a27a6cd386c
pusit <pusit@kptechman.com>
2023-11-04 23:06:52 +0700

    add extend

--- testconsole.js
+++ testconsole.js
@@ -1,3 +1,10 @@
 console.log(yea)
 console.log(2 more lines)
 console.log(1 more line)
+extends.define([
+    'require',
+    'dependency'
+], function(require, factory) {
+    'use strict';
+    
+});
